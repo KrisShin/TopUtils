b7c95f178ed8add2db263948dacb0f5b42942015
Kris <KrisShin5014@outlook.com>
2025-05-23 11:48:19 +0800

    client template.

--- merchant/src/api/upload/index.tsx
+++
@@ -1,9 +0,0 @@
-import axios from '../axios'
-
-export function uploadSubmit(sellerId: string | number, data: any) {
-    return axios({
-        url: '/seller/na/files/' + sellerId + '/upload',
-        method: 'post',
-        data
-    });
-}
